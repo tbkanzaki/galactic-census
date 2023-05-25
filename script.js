497424a1c17dc84bec551ee796c725f198bf41e4
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-05-25 18:01:39 -0300

    Tarefa 3: obter os dados de um planeta ao clicar no botão

--- script.js
+++ script.js
@@ -3,12 +3,40 @@ let result = document.getElementById('result');
 async function printData(){
   let res_planets = await fetch('https://swapi.dev/api/planets/?format=json');
   let {results} = await res_planets.json();
-  results.forEach(planet => {
+  let index = 1;
+  results.forEach((planet) => {
     let li = document.createElement('li');
       li.innerHTML = `<p>
-                        <button class="btn btn-outline-dark btn-sm">${planet.name}</button>
-                      </p>`
+                        <button onclick="printDetails(${index})" class="btn btn-outline-dark btn-sm">
+                        ${planet.name}
+                        </button>
+                      </p>
+                      <span id="detail${index}"></span>        
+                      `
       result.appendChild(li);
+      index += 1
     });
+}
 
+async function printDetails(id){
+  let data = await fetch("https://swapi.dev/api/planets/"+id);
+  let planet = await data.json();
+
+  let detail = document.getElementById("detail"+id); 
+  detail.innerHTML = `<dl>
+                        <dt>Planeta </dt>
+                        <dd>${planet.name}</dd>
+                        <dt>Clima</dt>
+                        <dd>${planet.climate}</dd>
+                        <dt>População</dt>
+                        <dd>${planet.population}</dd>
+                        <dt>Terreno</dt>
+                        <dd>${planet.terrain}</dd>
+                      </dl>`
+
+  if (detail.style.display === "block") {
+    detail.style.display = "none";
+  } else {
+    detail.style.display = "block";
+  }
 }
