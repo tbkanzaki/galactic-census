f9d44156d9ddf52ae716c026c91a46bc42db339a
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-05-25 13:14:58 -0300

    Tarefa 2: para cada planeta, criar um botão na tela com o nome do planeta.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let result = document.getElementById('result');  
+
 async function printData(){
   let res_planets = await fetch('https://swapi.dev/api/planets/?format=json');
   let {results} = await res_planets.json();
   results.forEach(planet => {
-    console.log(planet.name)
+    let li = document.createElement('li');
+      li.innerHTML = `<p>
+                        <button class="btn btn-outline-dark btn-sm">${planet.name}</button>
+                      </p>`
+      result.appendChild(li);
     });
 
 }
