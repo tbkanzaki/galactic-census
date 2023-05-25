eabc27094944b56a9bed90cf9fba7f3c90831bb5
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-05-25 12:45:51 -0300

    Ajustando Tarefa 1: exibir como uma lista

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 async function printData(){
   let res_planets = await fetch('https://swapi.dev/api/planets/?format=json');
   let {results} = await res_planets.json();
-  console.log(results);
+  results.forEach(planet => {
+    console.log(planet.name)
+    });
+
 }
