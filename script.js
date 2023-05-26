4c231f2fa9246fa3b61885e921d41d67de7533e8
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-05-25 22:32:23 -0300

    Tarefa 4:  busca por planetas

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-let result = document.getElementById('result');  
+let ol_list = document.getElementById('ol_list');  
+let p = document.getElementById("msg"); 
+let query = document.getElementById('q'); 
 
 async function printData(){
-  let res_planets = await fetch('https://swapi.dev/api/planets/?format=json');
-  let {results} = await res_planets.json();
+  clear_list();
   let index = 1;
-  results.forEach((planet) => {
-    let li = document.createElement('li');
+  for (let i = 1; i < 7; i++) {
+
+    let res_planets = await fetch("https://swapi.dev/api/planets/?page="+i);
+    let {results} = await res_planets.json();
+
+    results.forEach((planet) => {
+      let li = document.createElement('li');
       li.innerHTML = `<p>
                         <button onclick="printDetails(${index})" class="btn btn-outline-dark btn-sm">
-                        ${planet.name}
+                          ${planet.name}
                         </button>
                       </p>
                       <span id="detail${index}"></span>        
                       `
-      result.appendChild(li);
-      index += 1
-    });
+      ol_list.appendChild(li);
+      index += 1;
+      });
+  }
+  p.innerHTML = '';
+  query.value = '';
 }
 
 async function printDetails(id){
@@ -38,5 +47,41 @@ async function printDetails(id){
     detail.style.display = "none";
   } else {
     detail.style.display = "block";
+  }
+}
+
+async function printSearch(){
+  clear_list();
+  p.innerHTML = '';
+
+  let planet = await fetch("https://swapi.dev/api/planets/?search="+query.value);
+  let {results} = await planet.json();
+
+  if (results.length > 0 && query.value !== '' ){
+    results.forEach((planet) => {
+      let li = document.createElement('li');
+      li.innerHTML  = `<dl>
+                          <dt>Planeta </dt>
+                          <dd>${planet.name}</dd>
+                          <dt>Clima</dt>
+                          <dd>${planet.climate}</dd>
+                          <dt>População</dt>
+                          <dd>${planet.population}</dd>
+                          <dt>Terreno</dt>
+                          <dd>${planet.terrain}</dd>
+                        </dl>`
+      ol_list.appendChild(li);
+    });
+  }
+  else {
+    p.innerHTML = 'Nenhum resultado encontrado';
+  }
+}
+
+function clear_list(){
+  if (ol_list != null) {
+    while (ol_list.hasChildNodes()) {
+      ol_list.removeChild(ol_list.firstChild);
+    }
   }
 }
